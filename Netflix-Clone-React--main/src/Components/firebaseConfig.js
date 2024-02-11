fd687f478ac3bf9a2b5d0373ac9a322a1a55bb08
ashu singh <ashishkumarpramamnik20004@gmail.com>
2024-02-11 22:53:27 +0530

    Intial Change

---
+++ Netflix-Clone-React--main/src/Components/firebaseConfig.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+//import { initializeApp } from "firebase/app";
+//import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyCaO1arRi-iFv0MJ0ZexpfrV2ADhPyk7RQ",
+  authDomain: "netflix-5234a.firebaseapp.com",
+  projectId: "netflix-5234a",
+  storageBucket: "netflix-5234a.appspot.com",
+  messagingSenderId: "909603816937",
+  appId: "1:909603816937:web:015a72c58deef695833778",
+  measurementId: "G-C9BM2WHTNH"
+};
+
+// Initialize Firebase
+//const app = initializeApp(firebaseConfig);
+//const analytics = getAnalytics(app);
